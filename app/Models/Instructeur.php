f10ace2b4c7625f2d5aded4a582cfc71044cfa20
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-06-24 10:54:25 +0200

    create(model): implement delete function for instructeur

--- app/Models/Instructeur.php
+++ app/Models/Instructeur.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
+use Log;
 
 class Instructeur extends Model
 {
@@ -27,10 +29,27 @@ class Instructeur extends Model
     {
         try {
             // Gebruik statement voor updates/stored procedures zonder return data
-            return \DB::statement('CALL ToggleInstructeurStatus(?)', [$id]);
+            return DB::statement('CALL ToggleInstructeurStatus(?)', [$id]);
         } catch (\Exception $e) {
-            \Log::error('Error toggling status for Instructeur ID '.$id.': '.$e->getMessage());
+            Log::error('Error toggling status for Instructeur ID '.$id.': '.$e->getMessage());
             throw $e;
+        }
+    }
+
+    public function verwijderInstructeur($id)
+    {
+        try {
+            $res = DB::select('CALL deleteInstructeur(?)', [$id]);
+
+            if (is_array($res) && isset($res[0]) && isset($res[0]->status)) {
+                return $res[0]->status;
+            }
+
+            return 'error';
+        } catch (\Exception $e) {
+            Log::error('Fout in verwijderViaSP: '.$e->getMessage());
+
+            return 'error';
         }
     }
 }
